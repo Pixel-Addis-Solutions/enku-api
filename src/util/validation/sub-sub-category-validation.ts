e865eb7139460c9db1ddd21fcaefb7f95cdc28dc
AlemuTebkew <alemteb1010@gmail.com>
2024-12-16 21:22:53 +0300

    fix-validation

--- src/util/validation/sub-sub-category-validation.ts
+++ src/util/validation/sub-sub-category-validation.ts
@@ -3,12 +3,12 @@ import Joi from "joi";
 
 export const createSubSubCategorySchema = Joi.object({
   name: Joi.string().required(),
-  categoryId: Joi.number().required(),
-  subCategoryId: Joi.number().required(),
+  categoryId: Joi.string().required(),
+  subCategoryId: Joi.string().required(),
 });
 
 export const updateSubSubCategorySchema = Joi.object({
   name: Joi.string().required(),
-  categoryId: Joi.number().required(),
-  subCategoryId: Joi.number().required(),
+  categoryId: Joi.string().required(),
+  subCategoryId: Joi.string().required(),
 });
