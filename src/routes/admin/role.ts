320f1ad01be07d36f87bb1c667ec293c8ba2ca72
AlemuTebkew <alemteb1010@gmail.com>
2025-01-02 13:25:49 +0300

    fix:role-route

--- src/routes/admin/role.ts
+++ src/routes/admin/role.ts
@@ -6,8 +6,8 @@ import {
   getRoleById,
   updateRole,
   deleteRole,
-} from "../controllers/admin/role";
-  
+} from "../../controllers/admin/role";
+
 const router = Router();
 
 // Assign permissions to a role..
@@ -16,14 +16,11 @@ router.post("/:id/permissions", RoleController.assignPermissions);
 // Retrieve permissions for a specific role...
 router.get("/:id/permissions", RoleController.getRolePermissions);
 
-   
-
 // //role Crude Operation
 router.post("/", createRole); //to create role
 router.get("/", getAllRoles); //to get role
 router.get("/:id", getRoleById); //to get role by Id role
-router.put("/:id", updateRole);  //to update role
+router.put("/:id", updateRole); //to update role
 router.delete("/:id", deleteRole); //to delete role
-
 
 export default router;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,12 +2,11 @@ import { Router } from "express";
 import userRoutes from "./user/index";
 import adminRoutes from "./admin/index";
 import fileRoutes from "./file";
-import roleRoutes from "./role";
+import roleRoutes from "./admin/role";
 
 const router = Router();
 
 router.use("/user", userRoutes);
 router.use("/admin", adminRoutes);
 router.use("/files", fileRoutes);
-router.use("/api/roles", roleRoutes);
 export default router;
